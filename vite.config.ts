c94c6cde0f3fb11d49c659f1f7ca80ed54387d98
Douglas Figueiro <douglas.figueiro@ecomp.ufsm.br>
2025-12-28 21:49:25 -0300

    chore(pwa): centralize manifest via VitePWA, remove static manifests, use PNG icons, remove manifest link from index.html

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
         react(),
         VitePWA({
           registerType: 'autoUpdate',
-          includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+          includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'pwa-192x192.png', 'pwa-512x512.png'],
           manifest: {
             name: 'Track2Lift - Treino & Dieta',
             short_name: 'Track2Lift',
